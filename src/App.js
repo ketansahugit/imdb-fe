ff14fa97946b37ce9355a17f6d3ad234360b41a7
Ketan Sahu <rorozoro9888@gmail.com>
2023-09-15 16:04:49 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Header from "./components/header/Header";
 import Home from "./pages/home/Home.js";
+import MovieList from "./components/movieList/MovieList";
+import Movie from "./pages/movieDetail/movie";
 
 
 function App() {
@@ -11,8 +13,8 @@ function App() {
         <Header />
         <Routes>
           <Route index element={<Home />}></Route>
-          <Route path="movie/:id" element={<h1>Movie detail page</h1>}></Route>
-          <Route path="movies/:type" element={<h1>Movies list page</h1>}></Route>
+          <Route path="movie/:id" element={<Movie />}></Route>
+          <Route path="movies/:type" element={<MovieList />}></Route>
           <Route path="/*" element={<h1>Error Page</h1>}></Route>
         </Routes>
        </Router>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -3,6 +3,7 @@ import "./home.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import { Link } from 'react-router-dom';
+import MovieList from '../../components/movieList/MovieList';
 
 const Home = () => {
 const [popularMovies, setPopularMovies] = useState([])
@@ -43,6 +44,7 @@ const [popularMovies, setPopularMovies] = useState([])
             ))
           }
       </Carousel>
+      <MovieList />
      </div>
     </>
   )
